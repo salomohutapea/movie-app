cdf657629a45f1a7b835b2ab5f18a124e669db56
w <w>
2021-05-16 13:56:33 +0700

    Submission 3: Remove idlingresource from local datasource

--- app/src/main/java/com/example/movieapp/data/local/LocalDataSource.kt
+++ app/src/main/java/com/example/movieapp/data/local/LocalDataSource.kt
@@ -4,7 +4,6 @@ import com.example.movieapp.data.local.dao.MovieDao
 import com.example.movieapp.data.local.dao.TvShowDao
 import com.example.movieapp.data.model.Movie
 import com.example.movieapp.data.model.TvShow
-import com.example.movieapp.utils.EspressoIdlingResource
 
 class LocalDataSource private constructor(
     private val mMovieDao: MovieDao,
@@ -21,17 +20,13 @@ class LocalDataSource private constructor(
     }
 
     fun setFavoriteMovie(movie: Movie, newState: Boolean) {
-        EspressoIdlingResource.increment()
         movie.isFavorite = newState
         mMovieDao.update(movie)
-        EspressoIdlingResource.decrement()
     }
 
     fun setFavoriteTvShow(tvShow: TvShow, newState: Boolean) {
-        EspressoIdlingResource.increment()
         tvShow.isFavorite = newState
         mTvShowDao.update(tvShow)
-        EspressoIdlingResource.decrement()
     }
 
     fun insertMovies(movies: List<Movie>) = mMovieDao.insertMovies(movies)
