44e3775ffd8d652a9d19ff34fbcaef0865af6bac
salomohutapea <hutapeasalomo@gmail.com>
2021-05-24 18:16:20 +0700

    Submission 1 MADE : Add favorite recyclerview

---
+++ core/src/main/java/com/salomohutapea/movieapp/core/ui/FavoriteMovieAdapter.kt
@@ -0,0 +1,58 @@
+package com.salomohutapea.movieapp.core.ui
+
+import android.annotation.SuppressLint
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.salomohutapea.movieapp.core.R
+import com.salomohutapea.movieapp.core.databinding.ItemTvMovieSingleFavoriteBinding
+import com.salomohutapea.movieapp.core.domain.model.Movie
+
+class FavoriteMovieAdapter :
+    RecyclerView.Adapter<FavoriteMovieAdapter.ListViewHolder>() {
+
+    private var listData = ArrayList<Movie>()
+    private lateinit var onItemClickCallback: OnItemClickCallback
+
+    @SuppressLint("NotifyDataSetChanged")
+    fun setData(newListData: List<Movie>?) {
+        if (newListData == null) return
+        listData.clear()
+        listData.addAll(newListData)
+        notifyDataSetChanged()
+    }
+
+    fun setOnItemClickCallback(onItemClickCallback: OnItemClickCallback) {
+        this.onItemClickCallback = onItemClickCallback
+    }
+
+    override fun onCreateViewHolder(viewGroup: ViewGroup, i: Int): ListViewHolder {
+        val view: View = LayoutInflater.from(viewGroup.context)
+            .inflate(R.layout.item_tv_movie_single_favorite, viewGroup, false)
+        return ListViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ListViewHolder, position: Int) {
+        val movie = listData[position]
+        holder.itemView.setOnClickListener {
+            onItemClickCallback.onItemClicked(movie)
+        }
+        Glide.with(holder.itemView.context)
+            .load("https://image.tmdb.org/t/p/w500${movie.posterPath}")
+            .centerCrop()
+            .into(holder.binding.favSingleImage)
+    }
+
+    inner class ListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        internal var binding = ItemTvMovieSingleFavoriteBinding.bind(itemView)
+    }
+
+    interface OnItemClickCallback {
+        fun onItemClicked(data: Movie)
+    }
+
+    override fun getItemCount(): Int = listData.size
+
+}
--- favorite/src/main/java/com/salomohutapea/movieapp/favorite/FavoriteActivity.kt
+++ favorite/src/main/java/com/salomohutapea/movieapp/favorite/FavoriteActivity.kt
@@ -1,12 +1,22 @@
 package com.salomohutapea.movieapp.favorite
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.salomohutapea.movieapp.core.domain.model.Movie
+import com.salomohutapea.movieapp.core.domain.model.TvShow
+import com.salomohutapea.movieapp.core.ui.FavoriteMovieAdapter
+import com.salomohutapea.movieapp.core.ui.FavoriteTvShowAdapter
 import com.salomohutapea.movieapp.favorite.databinding.ActivityFavoriteBinding
+import com.salomohutapea.movieapp.moviedetail.MovieDetailActivity
+import com.salomohutapea.movieapp.tvdetail.TvDetailActivity
+import kotlinx.coroutines.DelicateCoroutinesApi
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import org.koin.core.context.loadKoinModules
 
+@DelicateCoroutinesApi
 class FavoriteActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityFavoriteBinding
@@ -28,16 +38,65 @@ class FavoriteActivity : AppCompatActivity() {
         showLoading(true)
         viewModel.isLoading.postValue(0)
         viewModel.getFavoriteMovies().observe(this, {
-            if(it != null) {
-                viewModel.isLoading.value = viewModel.isLoading.value?.plus(1)
+            if (it != null) {
+                insertDataMovie(it)
             }
+            viewModel.isLoading.value = viewModel.isLoading.value?.plus(1)
         })
         viewModel.getFavoriteTvShows().observe(this, {
+            if (it != null) {
+                insertDataTvShow(it)
+            }
             viewModel.isLoading.value = viewModel.isLoading.value?.plus(1)
         })
         viewModel.isLoading.observe(this, {
-            if(it == 2) {
+            if (it == 2) {
                 showLoading(false)
+            }
+        })
+    }
+
+    private fun insertDataMovie(list: List<Movie>) {
+        val _adapter = FavoriteMovieAdapter()
+        _adapter.setData(list)
+        with(binding.favRvMovie) {
+            adapter = _adapter
+            itemAnimator = null
+            setHasFixedSize(true)
+            val linearLayoutManager = LinearLayoutManager(this@FavoriteActivity)
+            linearLayoutManager.orientation = LinearLayoutManager.HORIZONTAL
+            layoutManager = linearLayoutManager
+        }
+
+        _adapter.setOnItemClickCallback(object : FavoriteMovieAdapter.OnItemClickCallback {
+            override fun onItemClicked(data: Movie) {
+                val intent = Intent(applicationContext, MovieDetailActivity::class.java)
+                intent.putExtra("MOVIE", data)
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                startActivity(intent)
+            }
+        })
+    }
+
+    private fun insertDataTvShow(list: List<TvShow>) {
+        val _adapter = FavoriteTvShowAdapter()
+        _adapter.setData(list)
+
+        with(binding.favRvTv) {
+            adapter = _adapter
+            itemAnimator = null
+            setHasFixedSize(true)
+            val linearLayoutManager = LinearLayoutManager(this@FavoriteActivity)
+            linearLayoutManager.orientation = LinearLayoutManager.HORIZONTAL
+            layoutManager = linearLayoutManager
+        }
+
+        _adapter.setOnItemClickCallback(object : FavoriteTvShowAdapter.OnItemClickCallback {
+            override fun onItemClicked(data: TvShow) {
+                val intent = Intent(applicationContext, TvDetailActivity::class.java)
+                intent.putExtra("TVSHOW", data)
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                startActivity(intent)
             }
         })
     }
